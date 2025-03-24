8bff6400d0faffc3f689d7cd3334afbbfee02727
Liquiid <liquiid727@outlook.com>
2025-03-24 18:16:16 +0800

    feat:add basic outline in each file

---
+++ pre_site/app/general/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <div>总览画面</div>
+    <div>
+    提供多个图类的展示数据情况
+    包含薪资情况 等的折线图 饼图等 
+    </div>
+    </>
+  )
+}
+
+export default page
--- pre_site/app/page.tsx
+++ pre_site/app/page.tsx
@@ -25,10 +25,10 @@ const nav=[
 export default function Home() {
   return (
     <>
-      <header className="page_header pl-24 pr-24 pt-8">
+      <header className="page_header  box-border pl-24 pr-24 pt-8 ">
         <div className="header_container flex place-content-between " >
           {/* 左侧栏目 */}
-          <div className="flex ">
+          <div className="left_container flex  items-end">
             {/* 个人IP logo */}
             <div className="logo_container w-12 h-12">
               <Image src="/assets/site_pra.png" alt="logo1" width={100} height={100} style={{ borderRadius: '50%' }} />
@@ -36,6 +36,9 @@ export default function Home() {
             {/* 站点IP logo */}
             <div className="logo_container w-12 h-12">
               <Image src="/assets/site_pra.png" alt="logo2" width={100} height={100} style={{ borderRadius: '50%' }} />
+            </div>
+            <div className="site_title_container site_name">
+              IT situation analytics
             </div>
 
           </div>
@@ -52,16 +55,23 @@ export default function Home() {
               </a>
             </div>
           </div>
-
         </div>
       </header>
-      <main className="page_main pl-24 pr-24">
-        <div>
-          IT situation analytics
+      <main className="page_main pl-24 pr-24 w-full h-screen ">
+        <div className=" ">
+           IT situation analytics main banner image placeholder
+           主站介绍大图/简要介绍内容和功能
+          * IT行情总览
+          * 更新时间
+          * 按段时间获取和分析BOSS直聘数据，根据数据分析和预测现有局势，分析未来职业走向
+          * 也可以为您根据行情方向，选择更加合适的提桶跑路的方向
+
         </div>
       </main>
-      <footer>
-
+      <footer className="page_footer pl-24 pr-24 w-full">
+        <div>Job situation forecast based on the data from BOSS Zhipin helps everyone make preparations for fallback options.</div>
+        <div>If this thing has been helpful to you, you might as well give it a star.</div>
+        <div>Welcome PRs to help improve this project.</div>
       </footer>
     </>
   );
